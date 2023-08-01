b4a122640701502f8983a5dd386b42813354151c
eunsonny <eunsonnny@gmail.com>
2023-08-01 22:58:23 +0900

    Add: appContext 생성

---
+++ src/appContext/index.ts
@@ -0,0 +1,14 @@
+export class AppContext {
+  // private api: HttpProviderDependencies;
+  // private storage: StorageProviderDependencies;
+  // private repositories: RepositoryDependencies;
+  // services: ServiceDependencies;
+
+  constructor() {
+    // 초기화 해준다.
+    // this.api = new HttpProviderDependencies()
+    // this.storage = new StorageProviderDependencies();
+    // this.repositories = new RepositoryDependencies();
+    // this.services = new ServicesDependencies(this.repositories);
+  }
+}
